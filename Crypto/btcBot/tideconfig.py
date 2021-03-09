3e70f9a2c82f99e29bb927ca5128d87ca1525547
Justin Boggs <jboggs1@gmail.com>
2021-03-09 10:53:13 -0600

    added crypto trading bot

---
+++ Crypto/btcBot/tideconfig.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+rh = {
+    "username": "xxxxx",
+    "password": "xxxxx",
+}
+
+config = {
+    "statusEmailAddress": "xxxxx",
+    "buyLimit": 0.0075,
+    "sellLimit": 0.01,
+    "movingAverageWindows": 24,    # 4 hours * 6 samples per hour
+    "runMinute": [0,5,10,15,20,25,30,35,40,45,50,55],
+    "coinList": ["BTC"],
+    "tradesEnabled": True,
+    "rsiWindow": 48,
+}
+
+
+
+
